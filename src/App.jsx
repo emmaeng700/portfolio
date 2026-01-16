a08580566c25d40bfce3eaa21cd682dfe95dce9c
Emmanuel Oppong <eoppong@gsumail.gram.edu>
2026-01-16 13:13:51 -0600

    working on setiing router paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import {Route, BrowserRouter as Router, Routes} from 'react-router-dom'
 import React from 'react'
 
 const App = () => {
   return (
-    <div className='text-3xl font-bold'>App</div>
+    <main className='bg-slate-300/20'>
+       <Router>
+        Navbar
+        <Routes>
+          <Route path='/' element={'Home'} />
+          <Route path='/about' element={'About'} />
+          <Route path='/projects' element={'Projects'} />
+          <Route path='/contact' element={'Contact'} />
+        </Routes>
+       </Router>
+    </main>
   )
 }
 
