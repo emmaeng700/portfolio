6e608d29ba31ea7d0d5205ed9f45358353cdc4d3
Emmanuel Oppong <eoppong@gsumail.gram.edu>
2026-01-16 13:23:34 -0600

    adding assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import {Route, BrowserRouter as Router, Routes} from 'react-router-dom'
 import React from 'react'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <main className='bg-slate-300/20'>
        <Router>
-        Navbar
+        <Navbar />
         <Routes>
           <Route path='/' element={'Home'} />
           <Route path='/about' element={'About'} />
