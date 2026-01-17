48390e7dc2a167c6ea4380c5b6e84657e9751b0f
Emmanuel Oppong <eoppong@gsumail.gram.edu>
2026-01-17 09:08:11 -0600

    added a home button for easy redirection to homepage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,6 +9,9 @@ const Navbar = () => {
         <img src={logo} alt='logo' className='w-18 h-18 object-contain' />
       </NavLink>
       <nav className='flex text-lg gap-7 font-medium'>
+        <NavLink to='/' className={({ isActive }) => isActive ? "text-blue-600" : "text-black" }>
+          Home
+        </NavLink>
         <NavLink to='/about' className={({ isActive }) => isActive ? "text-blue-600" : "text-black" }>
           About
         </NavLink>
